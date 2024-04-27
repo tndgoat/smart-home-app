aae020c67831581e8c0e2cce0449f7feecf7938a
Athena <bqh1402003@gmail.com>
2024-04-27 09:21:58 +0700

    feat: add auth controller

---
+++ backend/src/app/controller/auth.controller.js
@@ -0,0 +1,41 @@
+const io = require('socket.io-client');
+const AccountModel = require('../model/account.model');
+
+class AuthController {
+    constructor() {
+        this.socket = io('http://localhost:3000');
+
+        this.socket.on('connect', () => {
+            this.socket.emit('join controller room', this.name);
+        });
+
+        this.socket.on(`client to ${this.name}`, (message) => {
+            const { username, password } = JSON.parse(message);
+            let status = true;
+
+            AccountModel.findOne({ username: username })
+                .then((doc) => {
+                    if (!doc) {
+                        status = false;
+                    } else if (doc.password != password) {
+                        status = false;
+                    }
+                })
+                .then(() => {
+                    this.socket.emit('transmission', {
+                        from: 'authController',
+                        to: 'client',
+                        data: {
+                            status: status,
+                        },
+                    });
+                });
+        });
+    }
+
+    getSocket() {
+        return this.socket;
+    }
+}
+
+module.exports = AuthController;
