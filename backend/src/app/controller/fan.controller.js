bf2f3eaff0eebd9e1a7645f0bbe576dda8055549
Athena <bqh1402003@gmail.com>
2024-04-27 09:22:08 +0700

    feat: add fan controller

---
+++ backend/src/app/controller/fan.controller.js
@@ -0,0 +1,42 @@
+const io = require('socket.io-client');
+const { ADAFRUIT_IO_FEEDS } = require('../../config/adafruit');
+const MqttClient = require('../../utils/mqttClient');
+const Subscriber = require('../../utils/subscriber');
+const { DeviceModel, FanModel } = require('../model/device.model');
+
+class FanController extends Subscriber {
+    constructor(mqttClient, topic) {
+        super();
+        this.socket = io('http://localhost:3000');
+        this.name = 'fanController';
+
+        this.socket.on('connect', () => {
+            this.socket.emit('join controller room', this.name);
+        });
+
+        this.socket.on(`client to ${this.name}`, (message) => {
+            mqttClient.sendMessage(ADAFRUIT_IO_FEEDS + topic, message);
+        });
+    }
+
+    update(context) {
+        this.socket.emit('transmission', context);
+
+        DeviceModel.deleteMany({ type: 'Fan' })
+            .then(() => {
+                let model = new FanModel({
+                    status: context.data.status,
+                });
+                model.save().then(() => console.log('database is updated')); // Success
+            })
+            .catch(function (error) {
+                console.log(error); // Failure
+            });
+    }
+
+    getSocket() {
+        return this.socket;
+    }
+}
+
+module.exports = FanController;
