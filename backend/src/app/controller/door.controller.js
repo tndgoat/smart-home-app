9c32239630ed2cba747af1ba1cf12663af7d5c31
Athena <95176669+Athenaxdd@users.noreply.github.com>
2024-04-30 14:31:14 +0700

    Feat: Add door controller, subscribe MQTT (#2)
    * feat: init MQTT
    
    * feat: fix mqtt keys
    
    * feat: add thingsboard keys
    
    * feat: add auth controller
    
    * feat: add fan controller
    
    * feat: add fire controller
    
    * feat: add light controller
    
    * feat: remove api
    
    * Feat: Add door controller
    
    * Feat: subscribe mqtt to each module

--- backend/src/app/controller/door.controller.js
+++ backend/src/app/controller/door.controller.js
@@ -1,30 +1,30 @@
 const io = require('socket.io-client');
-const {THINGSBOARD_IO_FEEDS} = require('../../config/thingsboard');
+const { THINGSBOARD_IO_FEEDS } = require('../../config/thingsboard');
 const MqttClient = require('../../utils/mqttClient');
 const Subscriber = require('../../utils/subscriber');
-const { DeviceModel, LightModel } = require('../model/device.model');
+const { DeviceModel, DoorModel } = require('../model/device.model');
 
-class LightController extends Subscriber {
+class DoorController extends Subscriber {
     constructor(mqttClient, topic) {
         super();
         this.socket = io('http://localhost:3000');
-        this.name = 'lightController';
+        this.name = 'doorController';
 
         this.socket.on('connect', () => {
             this.socket.emit('join controller room', this.name);
         });
 
         this.socket.on(`client to ${this.name}`, (message) => {
-            mqttClient.sendMessage(ADAFRUIT_IO_FEEDS + topic, message);
+            mqttClient.sendMessage(THINGSBOARD_IO_FEEDS+ topic, message);
         });
     }
 
     update(context) {
         this.socket.emit('transmission', context);
 
-        DeviceModel.deleteMany({ type: 'Light' })
+        DeviceModel.deleteMany({ type: 'Door' })
             .then(() => {
-                let model = new LightModel({
+                let model = new DoorModel({
                     status: context.data.status,
                 });
                 model.save().then(() => console.log('database is updated')); // Success
@@ -39,4 +39,4 @@ class LightController extends Subscriber {
     }
 }
 
-module.exports = LightController;
+module.exports = DoorController;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,74 @@
+
+import { Server } from 'socket.io'
+import db from './config/database'
+import MqttClient from './utils/mqttClient'
 import AuthController from './app/controller/auth.controller'
 import FanController from './app/controller/fan.controller'
 import FireController from './app/controller/fire.controller'
+import LightController from './app/controller/light.controller'
+// import TemperatureController from './app/controller/temp.controller'
 
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-const port = 5003
 
-dotenv.config()
+import Subscriber from './utils/subscriber'
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+const io = new Server(3000)
+db.connect()
+
+const mqttClient = new MqttClient();
+const [
+    temperature,
+    door,
+    light,
+    fan,
+    fire,
+] = [
+    'temperature',
+    'door',
+    'light',
+    'fan',
+    'fire',
+].map((item) => ({ feed: `smarthome-${item}`, name: `${item}Controller` }));
+
+const authController = new AuthController();
+// const temperatureController = new TemperatureController();
+const lightController = new LightController(mqttClient, light.feed);
+const fanController = new FanController(mqttClient, fan.feed);
+const fireController = new FireController();
+
+mqttClient.subscribe(temperatureController, temperature.name)
+mqttClient.subscribeTopic(temperature.feed)
+mqttClient.subscribe(doorController, door.name)
+mqttClient.subscribeTopic(door.feed)
+mqttClient.subscribe(lightController, light.name)
+mqttClient.subscribeTopic(light.feed)
+mqttClient.subscribe(fanController, fan.name)
+mqttClient.subscribeTopic(fan.feed)
+mqttClient.subscribe(fireController, fire.name)
+mqttClient.subscribeTopic(fire.feed)
+
+io.on('connection', (socket) => {
+    socket.on('join controller room', (message) => {
+        socket.join(message)
+    })
+
+    socket.on('join client room', () => {
+        socket.join('client room')
+
+        io.in('client room')
+            .fetchSockets()
+            .then((sockets) => {
+                for (let socket of sockets) {
+                    console.log(socket.id)
+                }
+            })
+    })
+
+    socket.on('transmission', (message) => {
+        const { from, to, data } = message
+        io.to(`${from === 'client' ? to : 'client room'}`).emit(
+            `${from} to ${to}`,
+            JSON.stringify(data)
+        )
+        console.log(`Message from ${from} to ${to}: ${JSON.stringify(data)}`)
+    })
+})
